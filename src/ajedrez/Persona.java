87a3fc9811df8f0cf7dde9f077d8261755411152
betico <betico@betico-PC>
2014-01-03 12:48:02 +0100

    Añadidos clases Persona y Máquina

---
+++ src/ajedrez/Persona.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ajedrez;
+
+/**
+ *
+ * @author betico
+ */
+public class Persona extends Jugador{
+    
+}
