920414d96755a6fa5591f1e3701012e20c34e35e
betico <betico@betico-PC>
2014-01-03 12:43:24 +0100

    Añado nuevo paquete para implementar las piezas que heredan de la clase Pieza

---
+++ src/ajedrez/piezas/Peon.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ajedrez.piezas;
+
+import ajedrez.Movimientos;
+import ajedrez.Pieza;
+import ajedrez.Posicion;
+
+/**
+ *
+ * @author betico
+ */
+public class Peon extends Pieza{
+
+    @Override
+    public Movimientos[] getMovimientosPosibles() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean esMovimientoPosible(Posicion nuevoDestino) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void actualizarPosicion(Posicion nuevaPosicion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
