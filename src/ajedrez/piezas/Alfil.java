914dde17fb6b09f6d97393e48e7157f1aa1e1d0d
betic0 <betic0@betic0-ThinkPad-Edge>
2014-01-05 19:39:42 +0100

    Primeras pruebas para implementación de las piezas -> Pruebas hechas en Alfil.java

--- src/ajedrez/piezas/Alfil.java
+++ src/ajedrez/piezas/Alfil.java
@@ -16,6 +16,9 @@ public class Alfil extends Pieza{
     Posicion pos;
     int fila_actual;
     char columna_actual;
+    char[] filas = {'0','1','2','3','4','5','6','7','8'};
+    char[] columnas = {'a','b','c','d','e','f','g','h'};
+    Posicion[] result;
     public Alfil(){
         this.fila_actual = pos.getFila();
         this.columna_actual = pos.getColumna();
@@ -23,14 +26,78 @@ public class Alfil extends Pieza{
     
     @Override
     public Movimientos[] getMovimientosPosibles() {
+        //Al ser un alfil tenemos 4 posibles caminos que comprobar (las 4 esquinas)
+        int indice = 0;
+        //Esquina superior derecha
+        int f_aux = fila_actual;
+        int c_aux = new String(columnas).indexOf(columna_actual); //Obtenemos la posición dentro del array
+        while((f_aux < filas.length) && (c_aux < columnas.length)){
+            
+            f_aux++;
+            c_aux++;
+            result[indice].columna = columnas[c_aux];
+            result[indice].fila = f_aux;
+            indice++;
+        }
+        //Esquina inferior derecha
+        //Partimos del punto inicial para volver a mirar
+        f_aux = fila_actual;
+        c_aux = new String(columnas).indexOf(columna_actual);
+        while((f_aux > 0) && (c_aux < columnas.length)){       
+            f_aux--;
+            c_aux++;
+            result[indice].columna = columnas[c_aux];
+            result[indice].fila = f_aux;
+            indice++;
+        }
+        f_aux = fila_actual;
+        c_aux = new String(columnas).indexOf(columna_actual);
+        while((f_aux < filas.length) && (c_aux > 0)){
+            //Esquina superior izquierda
+            f_aux++;
+            c_aux--;
+            result[indice].columna = columnas[c_aux];
+            result[indice].fila = f_aux;
+            indice++;
+        }
+        f_aux = fila_actual;
+        c_aux = new String(columnas).indexOf(columna_actual);
+        while((f_aux > 0) && (c_aux > 0)){
+            //Esquina inferior izquierda
+            f_aux--;
+            c_aux--;
+            result[indice].columna = columnas[c_aux];
+            result[indice].fila = f_aux;
+            indice++;
+        }
         
+        //Aquí tenemos que devolver el array result pero ¿De qué tipo?
         return null;
         
     }
 
     @Override
     public boolean esMovimientoPosible(Posicion nuevoDestino) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //Aquí la idea es en primer lugar mirar en el array que nos ha devuelto 
+        //el método anterior si nuevoDestino está dentro de el y por tanto
+        //sería candidato a moverse si no hay otra ficha o alguna por medio
+        
+        Posicion[] array = null; // array de prueba 
+        boolean esposible = false;
+        int indice = 0; //indice para recorrer el array
+        while ((array[indice].fila != nuevoDestino.fila) || (array[indice].columna != nuevoDestino.columna))
+        {
+            indice++;
+        }
+        if (indice < array.length)
+        {
+            esposible = true;
+        }
+        
+        //Habría que comprobar aquí que hay fichas por medio o eso se haría en otro sitio?
+        
+        return esposible;
+        
     }
 
     @Override
