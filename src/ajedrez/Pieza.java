742dfb04abc7da0212822e23cc615cc8e4582dd7
betico <betico@betico-PC>
2014-01-12 19:27:45 +0100

    Creo constructor de Pieza2

--- src/ajedrez/Pieza.java
+++ src/ajedrez/Pieza.java
@@ -12,4 +12,14 @@ package ajedrez;
  */
 public abstract class Pieza implements IPieza {
     public static Posicion posicion;
+    public Color color;
+    
+    //implementacion del constructor de la clase Pieza
+    /* El contructor va a recibir la posicion
+     * de la pieza al crearla y su color
+     */
+    public Pieza(Posicion pos, Color col){
+        this.posicion = pos;
+        this.color = col;
+    }
 }
