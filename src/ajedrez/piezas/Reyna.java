e70d636500787e834ef2b58dd1d2ae39176693c4
chaudpotate <chaudpotate@hotmail.com>
2014-01-13 00:26:38 +0100

    Peon Rey Reyna Torre

--- src/ajedrez/piezas/Reyna.java
+++ src/ajedrez/piezas/Reyna.java
@@ -7,6 +7,7 @@ package ajedrez.piezas;
 import ajedrez.Color;
 import ajedrez.Movimientos;
 import ajedrez.Pieza;
+import static ajedrez.Pieza.posicion;
 import ajedrez.Posicion;
 
 /**
@@ -14,23 +15,152 @@ import ajedrez.Posicion;
  * @author betico
  */
 public class Reyna extends Pieza{
-
-    public Reyna(Posicion pos, Color col){
-        super(pos,col);
+    Movimientos mov;
+    //Posicion pos;
+    int fila_actual;
+    int columna_actual;
+    char[] filas = {'0','1','2','3','4','5','6','7','8'};
+    char[] columnas = {'a','b','c','d','e','f','g','h'};
+    Posicion[] result;
+    /**
+     *
+     */
+    
+    public Reyna(Posicion pos, Color c)
+    {
+        super(pos,c);
+        this.fila_actual = pos.getFila();
+        this.columna_actual = pos.getColumna();
+        this.color = c; 
     }
     @Override
-    public Movimientos getMovimientosPosibles() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Movimientos getMovimientosPosibles() 
+    {
+        //Al ser una reyna tenemos 8 posibles caminos que comprobar (las 4 esquinas y  los 4 horizaontales y verticales)
+        int indice = 0;
+        //Esquina superior derecha
+        int f_aux = fila_actual;
+        int c_aux = new String(columnas).indexOf(columna_actual); //Obtenemos la posición dentro del array
+        while((f_aux < filas.length) && (c_aux < columnas.length)){
+            
+            f_aux++;
+            c_aux++;
+            result[indice].columna = columnas[c_aux];
+            result[indice].fila = f_aux;
+            //result[indice] = c_aux;
+            mov.anadirMovimiento("reyna", Integer.toString(c_aux), Integer.toString(c_aux));
+            indice++;
+        }
+        //Esquina inferior derecha
+        //Partimos del punto inicial para volver a mirar
+        f_aux = fila_actual;
+        c_aux = new String(columnas).indexOf(columna_actual);
+        while((f_aux > 0) && (c_aux < columnas.length)){       
+            f_aux--;
+            c_aux++;
+            result[indice].columna = columnas[c_aux];
+            result[indice].fila = f_aux;
+            mov.anadirMovimiento("reyna", Integer.toString(c_aux), Integer.toString(c_aux));
+            indice++;
+        }
+        f_aux = fila_actual;
+        c_aux = new String(columnas).indexOf(columna_actual);
+        while((f_aux < filas.length) && (c_aux > 0)){
+            //Esquina superior izquierda
+            f_aux++;
+            c_aux--;
+            result[indice].columna = columnas[c_aux];
+            result[indice].fila = f_aux;
+            mov.anadirMovimiento("reyna", Integer.toString(c_aux), Integer.toString(c_aux));
+            indice++;
+        }
+        f_aux = fila_actual;
+        c_aux = new String(columnas).indexOf(columna_actual);
+        while((f_aux > 0) && (c_aux > 0)){
+            //Esquina inferior izquierda
+            f_aux--;
+            c_aux--;
+            result[indice].columna = columnas[c_aux];
+            result[indice].fila = f_aux;
+            mov.anadirMovimiento("reyna", Integer.toString(c_aux), Integer.toString(c_aux));
+            indice++;
+        }
+        
+        f_aux = fila_actual;
+        c_aux = new String(columnas).indexOf(columna_actual);
+        while(f_aux > 0){
+            //Hacia atras
+            f_aux--;
+            result[indice].columna = columnas[c_aux];
+            result[indice].fila = f_aux;
+            mov.anadirMovimiento("reyna", Integer.toString(c_aux), Integer.toString(c_aux));
+            indice++;
+        }
+        
+        f_aux = fila_actual;
+        c_aux = new String(columnas).indexOf(columna_actual);
+        while(f_aux < filas.length){
+            //Hacia delante
+            f_aux++;
+            result[indice].columna = columnas[c_aux];
+            result[indice].fila = f_aux;
+            mov.anadirMovimiento("reyna", Integer.toString(c_aux), Integer.toString(c_aux));
+            indice++;
+        }
+        f_aux = fila_actual;
+        c_aux = new String(columnas).indexOf(columna_actual);
+        while(c_aux > 0){
+            // Hacia izquiera
+            c_aux--;
+            result[indice].columna = columnas[c_aux];
+            result[indice].fila = f_aux;
+            mov.anadirMovimiento("reyna", Integer.toString(c_aux), Integer.toString(c_aux));
+            indice++;
+        }
+
+        f_aux = fila_actual;
+        c_aux = new String(columnas).indexOf(columna_actual);
+        while(c_aux < columnas.length){
+            // Hacia derecha
+            c_aux++;
+            result[indice].columna = columnas[c_aux];
+            result[indice].fila = f_aux;
+            mov.anadirMovimiento("reyna", Integer.toString(c_aux), Integer.toString(c_aux));
+            indice++;
+        } 
+       
+        
+        return mov;
     }
 
     @Override
     public boolean esMovimientoPosible(Posicion nuevoDestino) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //Aquí la idea es en primer lugar mirar en el array que nos ha devuelto 
+        //el método anterior si nuevoDestino está dentro de el y por tanto
+        //sería candidato a moverse si no hay otra ficha o alguna por medio
+        
+        Posicion[] array = null; // array de prueba 
+        boolean esposible = false;
+        int indice = 0; //indice para recorrer el array
+        while ((array[indice].fila != nuevoDestino.fila) || (array[indice].columna != nuevoDestino.columna))
+        {
+            indice++;
+        }
+        if (indice < array.length)
+        {
+            esposible = true;
+        }
+        
+        //Habría que comprobar aquí que hay fichas por medio o eso se haría en otro sitio?
+        
+        return esposible;
+        
     }
 
     @Override
     public void actualizarPosicion(Posicion nuevaPosicion) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        posicion.setColumna(nuevaPosicion.columna);
+        posicion.setFila(nuevaPosicion.fila);
     }
     
 }
